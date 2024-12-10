b65aa9a81198d4c7d4bde1b9fa0bc74815b09306
Papayadev2024 <diegomartinez.papaya@gmail.com>
2024-12-10 13:12:58 -0500

    final

--- resources/views/components/public/matrix.blade.php
+++ resources/views/components/public/matrix.blade.php
@@ -8,7 +8,16 @@
     <meta name="description" content="@yield('meta_description', 'Default Description')">
     <meta name="keywords" content="@yield('meta_keywords', 'default, keywords')">
     <title>@yield('meta_title', 'ALZAMORA ASOCIADOS')</title>
-    
+
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-NWCNNGTH');
+    </script>
+    <!-- End Google Tag Manager -->
+
     <!-- Google tag (gtag.js) -->
     <script async src="https://www.googletagmanager.com/gtag/js?id=G-80NRV03W4B"></script>
     <script>
@@ -52,6 +61,13 @@
 </head>
 
 <body class="body">
+    <!-- Google Tag Manager (noscript) -->
+    <noscript>
+        <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NWCNNGTH"
+            height="0" width="0" style="display:none;visibility:hidden"></iframe>
+        </noscript>
+    <!-- End Google Tag Manager (noscript) -->
+    
     {{-- <div class="overlay"></div> --}}
     @include('components.public.header')
 
